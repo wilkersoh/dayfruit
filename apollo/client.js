54b62b5e71e45d2e22712058581db41eb0bc2ce9
wilkersoh <plgu@hotmail.com>
2020-12-17 14:10:47 +0800

    add client endpoint

--- apollo/client.js
+++ apollo/client.js
@@ -184,8 +184,11 @@ function createApolloClient(initialState = {}, serverAccessToken) {
   const cache = new InMemoryCache().restore(initialState);
   // console.log(`process Status: ${process.env.NODE_ENV}`);
   const httpLink = new HttpLink({
-    uri: `https://dayfruit.staging.selfpaths.com/api/graphql`,
-    // url: "http://localhost:3000/api/graphql",
+    // uri: `https://dayfruit.staging.selfpaths.com/api/graphql`,
+    url:
+      process.env.NODE_ENV !== "production"
+        ? "http://localhost:3000/api/graphql"
+        : "https://dayfruit.staging.selfpaths.com/api/graphql",
     credentials: "include",
     fetch,
   });
