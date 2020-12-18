37acb3b3de709b36ac6dffcc45070795a122f326
wilkersoh <plgu@hotmail.com>
2020-12-18 15:14:58 +0800

    update categories ui

--- pages/fruits/[name].js
+++ pages/fruits/[name].js
@@ -35,24 +35,26 @@ export default function Fruits() {
 
   return (
     <App>
-      <Box>
+      <Box pl={{ lg: 6 }} pt={{ lg: 2 }}>
         {data.getFruitItems.map(({ name, country, createdAt }) => (
           <React.Fragment key={name}>
-            <Box height={{ sm: "300px", lg: "500px" }}>
-              <Box w='full' h='full' position='relative'>
-                <Image
-                  src='/images/01.jpg'
-                  alt='Picture of the fruit'
-                  layout='fill'
-                />
+            <Box d='flex' my={{ lg: 4 }} flexDir={{ sm: "column", lg: "row" }}>
+              <Box width={{ sm: "300px" }} height={{ sm: "300px" }}>
+                <Box w='full' h='full' position='relative'>
+                  <Image
+                    src='/images/01.jpg'
+                    alt='Picture of the fruit'
+                    layout='fill'
+                  />
+                </Box>
               </Box>
-            </Box>
-            <Box mt={2} mx={4} pb={4}>
-              <Box d='flex' justifyContent='space-between'>
-                <Text fontWeight='bold'>{name}</Text>
-                <Text>{format(parseISO(createdAt), "PP")}</Text>
+              <Box mt={{ sm: 2, lg: 0 }} mx={4} pb={4}>
+                <Box d='flex' justifyContent='space-between'>
+                  <Text fontWeight='bold'>{name}</Text>
+                  <Text>{format(parseISO(createdAt), "PP")}</Text>
+                </Box>
+                <Box>From: {country}</Box>
               </Box>
-              <Box>From: {country}</Box>
             </Box>
           </React.Fragment>
         ))}
